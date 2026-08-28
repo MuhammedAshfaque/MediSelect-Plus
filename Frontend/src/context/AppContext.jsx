3e427d45d822eb154dad7222e4168992ebd68faa
SK MD ASHFAQUE <ashfaquemuhammed94@gmail.com>
2026-08-28 19:36:12 +0530

    Update backend URL to production server

--- Frontend/src/context/AppContext.jsx
+++ Frontend/src/context/AppContext.jsx
@@ -7,8 +7,7 @@ export const AppContext = createContext()
 const AppContextProvider = (props) => {
 
     const currencySymbol = '₹'
-    // const backendUrl = "https://medi-select-backend.onrender.com"
-    const backendUrl = "http://localhost:4000"
+    const backendUrl = "https://medi-select-plus-backend.onrender.com"
 
     const [doctors, setDoctors] = useState([])
     const [token, setToken] = useState(localStorage.getItem('token') ? localStorage.getItem('token') : '')
