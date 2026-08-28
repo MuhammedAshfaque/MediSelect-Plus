bee24da2c806720bac602b92d2b20e831bd0a3f4
SK MD ASHFAQUE <ashfaquemuhammed94@gmail.com>
2026-08-28 20:01:28 +0530

    Remove duplicate CORS origin entry

--- Backend/server.js
+++ Backend/server.js
@@ -19,8 +19,7 @@ app.use(cors({
   origin: [
     "http://localhost:5174",
     "http://localhost:5173",
-    "https://medi-select-plus-frontend.onrender.com/"
-    "https://medi-select-admin.onrender.com",
+    "https://medi-select-plus-frontend.onrender.com"
   ],
   credentials: true
 }));
