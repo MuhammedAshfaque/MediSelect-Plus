4a756bf3cfd7de1aefdb8172ebcf6adacbfbd095
SK MD ASHFAQUE <ashfaquemuhammed94@gmail.com>
2026-08-28 20:00:06 +0530

    Update CORS origin for frontend URL

--- Backend/server.js
+++ Backend/server.js
@@ -19,7 +19,7 @@ app.use(cors({
   origin: [
     "http://localhost:5174",
     "http://localhost:5173",
-    "https://medi-select-frontend.onrender.com",
+    "https://medi-select-plus-frontend.onrender.com/"
     "https://medi-select-admin.onrender.com",
   ],
   credentials: true
