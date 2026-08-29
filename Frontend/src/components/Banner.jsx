6f9258be63f65ca74fcdd7703ef3ce96c0425416
Ashfaque <ashfaquemuhammed94@gmail.com>
2026-08-29 18:15:24 +0530

    Banner changed

--- Frontend/src/components/Banner.jsx
+++ Frontend/src/components/Banner.jsx
@@ -3,28 +3,6 @@ import { assets } from '../assets/assets'
 import { useNavigate } from 'react-router-dom'
 import { useContext } from 'react'
 import { AppContext } from '../context/AppContext'
-// const Banner = () => {
-//     const navigate = useNavigate()
-//     const { token } = useContext(AppContext)
-//   return (
-//     <div className='flex bg-primary rounded-lg px-6 sm:px-10 md:px-14 lg:px-12 my-20 mx-10'>
-//       {/* ------Left side------ */}
-//       <div className='flex-1 py-8 sm:py-10 md:py-16 lg:py-24 lg:pl-5'>
-//         <div className='text-xl sm:text-2xl md:text-3xl lg:text-5xl font-semibold text-white'>
-//             <p>Book Appoinment</p>
-//             <p className='mt-4'>With 100+ Trusted Doctors</p>
-//         </div>
-//         {!token && <button onClick={()=>{ navigate('/login'); scrollTo(0,0)}} className='bg-white text-sm sm:text-base text-gray-600 px-8 py-3 rounded-full mt-6 hover:scale-98 transition-all cursor-pointer'>Creat account</button>}
-//         {token && <button className='bg-white text-sm sm:text-base text-gray-600 px-8 py-3 rounded-full mt-6 hover:scale-98 transition-all cursor-pointer' onClick={()=> {navigate('/doctor'); scrollTo(0,0)}}>Book Appoinment</button>}
-//       </div>
-//       {/* ------Right side------ */}
-//       <div className='hidden md:block relative md:w-1/2 lg:w-[420px] h-[360px]'>
-//         <img className='absolute bottom-0 right-0 w-full h-auto' src={assets.appointment_img} alt="" />
-//       </div>
-//     </div>
-//   )
-// }
-
 const Banner = () => {
   const navigate = useNavigate()
   const { token } = useContext(AppContext)
